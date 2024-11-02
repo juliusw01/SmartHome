e6e5939a601d9fa912898a1b54c2790e5ccdedd0
juliusw01 <95189236+juliusw01@users.noreply.github.com>
2024-11-02 16:39:11 +0100

    fix

--- src/main/java/com/katzenklappe/smartHome/config/Secrets.java
+++ src/main/java/com/katzenklappe/smartHome/config/Secrets.java
@@ -7,17 +7,14 @@ import java.util.Properties;
 public class Secrets {
 
     public static String getPASSWORD() {
-        System.out.println("Retrieving PW");
         return getProperty("PASSWORD");
     }
 
     public static String getUSERNAME() {
-        System.out.println("Retrieving Username");
         return getProperty("USERNAME");
     }
 
     public static String getAUTH_TOKEN(){
-        System.out.println("Retrieving API Key");
         return getProperty("AUTH_TOKEN");
     }
 
@@ -27,10 +24,8 @@ public class Secrets {
             FileReader reader = new FileReader("secrets.properties");
             prop.load(reader);
             reader.close();
-            System.out.println("Property: " + prop.getProperty(propName));
             return prop.getProperty(propName);
         }catch (IOException e){
-            System.out.println("Error retrieving: ");
             System.out.println(e.getMessage());
             return e.getMessage();
         }
