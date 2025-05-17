da7c41f5aa6851cab11ff6345cfcb3822ad0d7da
juliusw01 <95189236+juliusw01@users.noreply.github.com>
2025-05-17 18:17:58 +0200

    remove AbstractHttpSessionApplicationInitializer --> not necessary due to embedded servlet container

--- gateway/src/main/java/com/katzenklappe/gateway/SessionConfig.java
+++ gateway/src/main/java/com/katzenklappe/gateway/SessionConfig.java
@@ -2,9 +2,8 @@ package com.katzenklappe.gateway;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
 @EnableRedisWebSession
-public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+public class SessionConfig {
 }
